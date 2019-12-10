2f6a87046760937baef232a8235eab46982d396b
Stian Grenborgen <stian.grenborgen@nav.no>
2019-12-10 10:15:16 +0100

    Endrer versjonsnummer siden fp-felles ikke støtter plusstegn før sha (selv om dette er semver).

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -3,4 +3,4 @@
 versionLine="$(grep '<revision>' pom.xml)"
 version="$(echo $versionLine | sed 's/<revision>\(.*\)<\/revision>/\1/g')"
 sha="$(git rev-parse --short HEAD)"
-echo "${version}+${sha}"
+echo "${version}.${sha}"
