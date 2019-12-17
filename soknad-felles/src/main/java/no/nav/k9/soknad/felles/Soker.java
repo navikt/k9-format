d85782fa93a5348137b2367e7f30e9a3bfcac8d2
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-12-17 12:38:32 +0100

    Fjerner språk fra søker

--- soknad-felles/src/main/java/no/nav/k9/soknad/felles/Soker.java
+++ soknad-felles/src/main/java/no/nav/k9/soknad/felles/Soker.java
@@ -3,12 +3,10 @@ package no.nav.k9.soknad.felles;
 public class Soker {
 
     private NorskIdentitetsnummer norskIdentitetsnummer;
-    private Spraak spraakValg;
 
     public Soker() {
 
     }
-
 
     public void setNorskIdentitetsnummer(NorskIdentitetsnummer norskIdentitetsnummer) {
         this.norskIdentitetsnummer = norskIdentitetsnummer;
@@ -16,13 +14,5 @@ public class Soker {
 
     public NorskIdentitetsnummer getNorskIdentitetsnummer() {
         return norskIdentitetsnummer;
-    }
-
-    public void setSpraakValg(Spraak spraakValg) {
-        this.spraakValg = spraakValg;
-    }
-
-    public Spraak getSpraakValg() {
-        return spraakValg;
     }
 }
