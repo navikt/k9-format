3338ac1b227ceb1bb40810c3f8d8e7583fa7a573
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2019-12-18 13:33:47 +0100

    Byttet ut maven deploy 'hack'

--- scripts/get-version.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-versionLine="$(grep '<revision>' pom.xml)"
-version="$(echo $versionLine | sed 's/<revision>\(.*\)<\/revision>/\1/g')"
-sha="$(git rev-parse --short HEAD)"
-echo "${version}.${sha}"
