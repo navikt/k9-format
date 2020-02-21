78a4987bdf99318888ef952b9b7cc7708ce7f44e
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-02-21 09:05:44 +0100

    Legger til SerDes-klasser i søknadene

--- soknad-pleiepenger-barn/src/test/java/no/nav/k9/søknad/pleiepengerbarn/PleiepengerBarnSøknadTest.java
+++ soknad-pleiepenger-barn/src/test/java/no/nav/k9/søknad/pleiepengerbarn/PleiepengerBarnSøknadTest.java
@@ -1,6 +1,5 @@
 package no.nav.k9.søknad.pleiepengerbarn;
 
-import no.nav.k9.søknad.JsonUtils;
 import org.json.JSONException;
 import org.junit.Test;
 
@@ -11,13 +10,13 @@ public class PleiepengerBarnSøknadTest {
     public void serialiseringAvJsonOgBrukAvBuilderGirSammeResultat() throws JSONException {
         String json = TestUtils.jsonForKomplettSøknad();
         PleiepengerBarnSøknad fraBuilder = TestUtils.komplettBuilder().build();
-        JSONAssert.assertEquals(json, JsonUtils.toString(fraBuilder), true);
+        JSONAssert.assertEquals(json, PleiepengerBarnSøknad.SerDes.serialize(fraBuilder), true);
     }
 
     @Test
     public void reserialisering() throws JSONException {
         String json = TestUtils.jsonForKomplettSøknad();
-        PleiepengerBarnSøknad søknad = JsonUtils.fromString(json, PleiepengerBarnSøknad.class);
-        JSONAssert.assertEquals(json, JsonUtils.toString(søknad), true);
+        PleiepengerBarnSøknad søknad = PleiepengerBarnSøknad.SerDes.deserialize(json);
+        JSONAssert.assertEquals(json, PleiepengerBarnSøknad.SerDes.serialize(søknad), true);
     }
 }
