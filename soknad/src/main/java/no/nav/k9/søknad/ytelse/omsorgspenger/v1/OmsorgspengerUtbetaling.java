5b7d0f271cc0072a1126d13e3e04bdcec0b5b7a0
Frode Carlsen <frode.odde.carlsen@nav.no>
2021-01-20 18:57:48 +0100

    fix: utvider ytelse med søknadsperiode (#86)

--- soknad/src/main/java/no/nav/k9/søknad/ytelse/omsorgspenger/v1/OmsorgspengerUtbetaling.java
+++ soknad/src/main/java/no/nav/k9/søknad/ytelse/omsorgspenger/v1/OmsorgspengerUtbetaling.java
@@ -1,6 +1,18 @@
 package no.nav.k9.søknad.ytelse.omsorgspenger.v1;
 
-import com.fasterxml.jackson.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
 import no.nav.k9.søknad.PeriodeValidator;
 import no.nav.k9.søknad.felles.Feil;
 import no.nav.k9.søknad.felles.aktivitet.ArbeidAktivitet;
@@ -8,15 +20,10 @@ import no.nav.k9.søknad.felles.fravær.FraværPeriode;
 import no.nav.k9.søknad.felles.personopplysninger.Barn;
 import no.nav.k9.søknad.felles.personopplysninger.Bosteder;
 import no.nav.k9.søknad.felles.personopplysninger.Utenlandsopphold;
+import no.nav.k9.søknad.felles.type.Periode;
 import no.nav.k9.søknad.felles.type.Person;
 import no.nav.k9.søknad.ytelse.Ytelse;
 import no.nav.k9.søknad.ytelse.YtelseValidator;
-
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import java.util.ArrayList;
-import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.NONE, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE, creatorVisibility = JsonAutoDetect.Visibility.NONE)
@@ -39,13 +46,13 @@ public class OmsorgspengerUtbetaling implements Ytelse {
     @Valid
     @JsonProperty(value = "utenlandsopphold")
     private Utenlandsopphold utenlandsopphold;
-    
+
     @Valid
     @NotNull
-    @Size(min = 1, message="Minst 1 fraværsperiode må oppgis")
+    @Size(min = 1, message = "Minst 1 fraværsperiode må oppgis")
     @JsonProperty(value = "fraværsperioder", required = true)
     private final List<FraværPeriode> fraværsperioder;
-    
+
     @JsonCreator
     public OmsorgspengerUtbetaling(@JsonProperty("fosterbarn") @Valid List<Barn> fosterbarn,
                                    @JsonProperty(value = "aktivitet", required = true) @Valid @NotNull ArbeidAktivitet opptjening,
@@ -70,6 +77,14 @@ public class OmsorgspengerUtbetaling implements Ytelse {
         return fraværsperioder;
     }
 
+    /** har ikke egen søknadsperiode for omsorgspenger, men angir på bakggrunn av angitte fraværsperioder. */
+    @Override
+    public Periode getSøknadsperiode() {
+        var fom = fraværsperioder.stream().min(FraværPeriode::compareTo).orElseThrow().getPeriode().getFraOgMed();
+        var tom = fraværsperioder.stream().max(FraværPeriode::compareTo).orElseThrow().getPeriode().getTilOgMed();
+        return new Periode(fom, tom);
+    }
+
     @Override
     public Type getType() {
         return Type.OMSORGSPENGER_UTBETALING;
@@ -79,25 +94,27 @@ public class OmsorgspengerUtbetaling implements Ytelse {
     public List<Person> getBerørtePersoner() {
         return List.copyOf(fosterbarn);
     }
-    
+
     @Override
     public YtelseValidator getValidator() {
         return new OmsorgspengerUtbetalingValidator();
     }
 
-    @Size(max=0, message="${validatedValue}")
+    @Size(max = 0, message = "${validatedValue}")
     private List<Feil> validerAngittUtenlandsopphold() {
-        if (utenlandsopphold == null) List.of();
+        if (utenlandsopphold == null)
+            List.of();
         return new PeriodeValidator().validerIkkeTillattOverlapp(utenlandsopphold.perioder, "utenlandsopphold.perioder");
     }
 
-    @Size(max=0, message="${validatedValue}")
+    @Size(max = 0, message = "${validatedValue}")
     private List<Feil> validerAngittBosteder() {
-        if (bosteder == null) return List.of();
+        if (bosteder == null)
+            return List.of();
         return new PeriodeValidator().validerIkkeTillattOverlapp(bosteder.perioder, "bosteder.perioder");
     }
 
-    @Size(max=0, message="${validatedValue}")
+    @Size(max = 0, message = "${validatedValue}")
     private List<Feil> getValiderAngittFosterbarn() {
         var barn = this.fosterbarn;
         if (barn == null || barn.isEmpty())
