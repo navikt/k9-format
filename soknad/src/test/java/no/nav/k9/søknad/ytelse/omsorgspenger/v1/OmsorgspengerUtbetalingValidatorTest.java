d3f461fddc4ef0e7d33e9bfb8b89a581b0253d78
s149212 <espen.svendsen@nav.no>
2021-08-27 13:23:36 +0200

    Legger til SøknadÅrsak på FraværPeriode OMP

--- soknad/src/test/java/no/nav/k9/søknad/ytelse/omsorgspenger/v1/OmsorgspengerUtbetalingValidatorTest.java
+++ soknad/src/test/java/no/nav/k9/søknad/ytelse/omsorgspenger/v1/OmsorgspengerUtbetalingValidatorTest.java
@@ -111,6 +111,7 @@ class OmsorgspengerUtbetalingValidatorTest {
                                     null,
                                     null,
                                     FraværÅrsak.ORDINÆRT_FRAVÆR,
+                                    null,
                                     List.of(AktivitetFravær.SELVSTENDIG_VIRKSOMHET),
                                     null
                             )
