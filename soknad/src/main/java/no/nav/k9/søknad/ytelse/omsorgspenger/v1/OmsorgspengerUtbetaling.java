254a978f8fde170c53d8b6581d1dde491feb40a7
s149212 <espen.svendsen@nav.no>
2021-10-11 17:12:41 +0200

    Flytter fraværskorrigering IM inn under ytelse for OmsorgspengerUtbetaling
    Gjenbruker patternet for delvis søknadsinnhold som også anvendes av PSB

--- soknad/src/main/java/no/nav/k9/søknad/ytelse/omsorgspenger/v1/OmsorgspengerUtbetaling.java
+++ soknad/src/main/java/no/nav/k9/søknad/ytelse/omsorgspenger/v1/OmsorgspengerUtbetaling.java
@@ -1,12 +1,11 @@
 package no.nav.k9.søknad.ytelse.omsorgspenger.v1;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
@@ -14,15 +13,13 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
-import no.nav.k9.søknad.PeriodeValidator;
-import no.nav.k9.søknad.felles.Feil;
-import no.nav.k9.søknad.felles.opptjening.OpptjeningAktivitet;
 import no.nav.k9.søknad.felles.fravær.FraværPeriode;
+import no.nav.k9.søknad.felles.opptjening.OpptjeningAktivitet;
 import no.nav.k9.søknad.felles.personopplysninger.Barn;
 import no.nav.k9.søknad.felles.personopplysninger.Bosteder;
 import no.nav.k9.søknad.felles.personopplysninger.Utenlandsopphold;
-import no.nav.k9.søknad.felles.type.Person;
 import no.nav.k9.søknad.felles.type.Periode;
+import no.nav.k9.søknad.felles.type.Person;
 import no.nav.k9.søknad.ytelse.Ytelse;
 import no.nav.k9.søknad.ytelse.YtelseValidator;
 
@@ -36,8 +33,7 @@ public class OmsorgspengerUtbetaling implements Ytelse {
     private List<Barn> fosterbarn;
 
     @Valid
-    @NotNull
-    @JsonProperty(value = "aktivitet", required = true)
+    @JsonProperty(value = "aktivitet")
     private OpptjeningAktivitet aktivitet;
 
     @Valid
@@ -49,10 +45,12 @@ public class OmsorgspengerUtbetaling implements Ytelse {
     private Utenlandsopphold utenlandsopphold;
 
     @Valid
-    @NotNull
-    @Size(min = 1, message = "Minst 1 fraværsperiode må oppgis")
-    @JsonProperty(value = "fraværsperioder", required = true)
+    @JsonProperty(value = "fraværsperioder")
     private List<FraværPeriode> fraværsperioder;
+
+    @Valid
+    @JsonProperty(value = "fraværsperioderKorrigeringIm")
+    private List<FraværPeriode> fraværsperioderKorrigeringIm;
 
     public OmsorgspengerUtbetaling() {
         //
@@ -60,13 +58,15 @@ public class OmsorgspengerUtbetaling implements Ytelse {
 
     @JsonCreator
     public OmsorgspengerUtbetaling(@JsonProperty("fosterbarn") @Valid List<Barn> fosterbarn,
-                                   @JsonProperty(value = "aktivitet", required = true) @Valid @NotNull OpptjeningAktivitet opptjening,
-                                   @JsonProperty(value = "fraværsperioder", required = true) @Valid @NotNull @Size(min = 1) List<FraværPeriode> fraværsperioder,
-                                   @JsonProperty(value = "bosteder") @Valid @NotNull Bosteder bosteder,
-                                   @JsonProperty(value = "utenlandsopphold") @Valid @NotNull Utenlandsopphold utenlandsopphold) {
+                                   @JsonProperty(value = "aktivitet") @Valid OpptjeningAktivitet opptjening,
+                                   @JsonProperty(value = "fraværsperioder") @Valid List<FraværPeriode> fraværsperioder,
+                                   @JsonProperty(value = "fraværsperioderKorrigeringIm") @Valid List<FraværPeriode> fraværsperioderKorrigeringIm,
+                                   @JsonProperty(value = "bosteder") @Valid Bosteder bosteder,
+                                   @JsonProperty(value = "utenlandsopphold") @Valid Utenlandsopphold utenlandsopphold) {
         this.fosterbarn = fosterbarn;
         this.aktivitet = opptjening;
         this.fraværsperioder = fraværsperioder;
+        this.fraværsperioderKorrigeringIm = fraværsperioderKorrigeringIm;
         this.bosteder = bosteder;
         this.utenlandsopphold = utenlandsopphold;
     }
@@ -81,6 +81,10 @@ public class OmsorgspengerUtbetaling implements Ytelse {
 
     public List<FraværPeriode> getFraværsperioder() {
         return fraværsperioder == null? null: Collections.unmodifiableList(fraværsperioder);
+    }
+
+    public List<FraværPeriode> getFraværsperioderKorrigeringIm() {
+        return fraværsperioderKorrigeringIm;
     }
 
     public Bosteder getBosteder() {
@@ -104,8 +108,18 @@ public class OmsorgspengerUtbetaling implements Ytelse {
     /** har ikke egen søknadsperiode for omsorgspenger, men angir på bakggrunn av angitte fraværsperioder. */
     @Override
     public Periode getSøknadsperiode() {
-        var fom = fraværsperioder.stream().min(FraværPeriode::compareTo).orElseThrow().getPeriode().getFraOgMed();
-        var tom = fraværsperioder.stream().max(FraværPeriode::compareTo).orElseThrow().getPeriode().getTilOgMed();
+        LocalDate fom;
+        LocalDate tom;
+        if (fraværsperioder != null && !fraværsperioder.isEmpty()) {
+            fom = fraværsperioder.stream().min(FraværPeriode::compareTo).orElseThrow().getPeriode().getFraOgMed();
+            tom = fraværsperioder.stream().max(FraværPeriode::compareTo).orElseThrow().getPeriode().getTilOgMed();
+        } else if (fraværsperioderKorrigeringIm != null && !fraværsperioderKorrigeringIm.isEmpty()) {
+            fom = fraværsperioderKorrigeringIm.stream().min(FraværPeriode::compareTo).orElseThrow().getPeriode().getFraOgMed();
+            tom = fraværsperioderKorrigeringIm.stream().max(FraværPeriode::compareTo).orElseThrow().getPeriode().getTilOgMed();
+        } else {
+            // Skal fanges opp av validator
+            throw new IllegalArgumentException("Må ha fravær fra enten søker eller fra fraværskorrigering av inntektsmelding");
+        }
         return new Periode(fom, tom);
     }
 
@@ -151,39 +165,5 @@ public class OmsorgspengerUtbetaling implements Ytelse {
     public OmsorgspengerUtbetaling medUtenlandsopphold(Utenlandsopphold utenlandsopphold) {
         this.utenlandsopphold = utenlandsopphold;
         return this;
-    }
-
-    @Size(max = 0, message = "${validatedValue}")
-    private List<Feil> validerAngittUtenlandsopphold() {
-        if (utenlandsopphold == null)
-            List.of();
-        return new PeriodeValidator().validerIkkeTillattOverlapp(utenlandsopphold.getPerioder(), "utenlandsopphold.perioder");
-    }
-
-    @Size(max = 0, message = "${validatedValue}")
-    private List<Feil> validerAngittBosteder() {
-        if (bosteder == null)
-            return List.of();
-        return new PeriodeValidator().validerIkkeTillattOverlapp(bosteder.getPerioder(), "bosteder.perioder");
-    }
-
-    @Size(max = 0, message = "${validatedValue}")
-    private List<Feil> getValiderAngittFosterbarn() {
-        var barn = this.fosterbarn;
-        if (barn == null || barn.isEmpty())
-            return List.of();
-        var index = 0;
-        List<Feil> feil = new ArrayList<>();
-        for (Barn b : barn) {
-            if (b.getPersonIdent() == null && b.getFødselsdato() == null) {
-                feil.add(new Feil("fosterbarn[" + index + "]", "norskIdentitetsnummerEllerFødselsdatoPåkrevd",
-                    "Må sette enten Personnummer/D-nummer på fosterbarn, eller fødselsdato."));
-            } else if (b.getPersonIdent() != null && b.getFødselsdato() != null) {
-                feil.add(
-                    new Feil("fosterbarn[" + index + "]", "ikkeEntydigIdPåBarnet", "Må sette enten Personnummer/D-nummer på fosterbarn, eller fødselsdato."));
-            }
-            index++;
-        }
-        return feil;
     }
 }
--- soknad/src/main/java/no/nav/k9/søknad/ytelse/omsorgspenger/v1/OmsorgspengerUtbetalingValidator.java
+++ soknad/src/main/java/no/nav/k9/søknad/ytelse/omsorgspenger/v1/OmsorgspengerUtbetalingValidator.java
@@ -1,13 +1,20 @@
 package no.nav.k9.søknad.ytelse.omsorgspenger.v1;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import no.nav.k9.søknad.PeriodeValidator;
-import no.nav.k9.søknad.Validator;
 import no.nav.k9.søknad.felles.Feil;
+import no.nav.k9.søknad.felles.fravær.AktivitetFravær;
+import no.nav.k9.søknad.felles.fravær.FraværPeriode;
 import no.nav.k9.søknad.felles.opptjening.Frilanser;
 import no.nav.k9.søknad.felles.opptjening.SelvstendigNæringsdrivende;
+import no.nav.k9.søknad.felles.personopplysninger.Barn;
+import no.nav.k9.søknad.felles.type.Organisasjonsnummer;
 import no.nav.k9.søknad.ytelse.Ytelse;
 import no.nav.k9.søknad.ytelse.YtelseValidator;
 
@@ -23,44 +30,67 @@ public class OmsorgspengerUtbetalingValidator extends YtelseValidator {
         var omsorgspengerUtbetaling = (OmsorgspengerUtbetaling) ytelse;
 
         List<Feil> feil = new ArrayList<>();
-        getAktivitet(omsorgspengerUtbetaling, feil);
-        feil.addAll(Validator.validerTilFeil(ytelse));
+        feil.addAll(validerPeriodeInnenforEttÅr(omsorgspengerUtbetaling));
+        feil.addAll(validerAktivitet(omsorgspengerUtbetaling));
+        feil.addAll(validerUtenlandsopphold(omsorgspengerUtbetaling));
+        feil.addAll(validerFosterbarn(omsorgspengerUtbetaling));
+        feil.addAll(validerBosteder(omsorgspengerUtbetaling));
+        feil.addAll(validerFraværsperioderFraSøker(omsorgspengerUtbetaling));
+        feil.addAll(validerFraværskorrigeringIm(omsorgspengerUtbetaling));
+        feil.addAll(validerFraværsperiodeKilder(omsorgspengerUtbetaling));
+
         return feil;
     }
 
-    private void getAktivitet(OmsorgspengerUtbetaling søknad, List<Feil> feil) {
-        var aktivitet = søknad.getAktivitet();
+    private List<Feil> validerPeriodeInnenforEttÅr(OmsorgspengerUtbetaling ytelse) {
+        List<Feil> feil = new ArrayList<>();
+        var søknadsperiode = ytelse.getSøknadsperiode();
+        var yearMin = søknadsperiode.getFraOgMed().getYear();
+        var yearMax = søknadsperiode.getTilOgMed().getYear();
 
-        if (aktivitet != null) {
-
-            if ((aktivitet.getFrilanser() != null)
-                    || ((aktivitet.getSelvstendigNæringsdrivende() != null) && !aktivitet.getSelvstendigNæringsdrivende().isEmpty())) {
-                validerFrilanserOgSelvstendingNæringsdrivende(aktivitet.getSelvstendigNæringsdrivende(), aktivitet.getFrilanser(), feil);
-                validerSelvstendingNæringsdrivende(aktivitet.getSelvstendigNæringsdrivende(), feil);
-                // validerFrilanser(aktivitet.getFrilanser(), feil); // TODO: 24/03/2021 Bør aktiveres senere når søknadsdialogen er prodsatt med frilanser.sluttdato feltet.
-            }
+        if (yearMin != yearMax) {
+            feil.add(new Feil("fraværsperioder", "perioderOverFlereÅr", "Perioder kan ikke overstige ett år"));
         }
+        return feil;
     }
 
-    private void validerFrilanserOgSelvstendingNæringsdrivende(List<SelvstendigNæringsdrivende> selvstendigeVirksomheter, Frilanser frilanser,
-                                                               List<Feil> feil) {
+    private List<Feil> validerAktivitet(OmsorgspengerUtbetaling ytelse) {
+        List<Feil> feil = new ArrayList<>();
+        var aktivitet = ytelse.getAktivitet();
+
+        if (aktivitet != null) {
+            if ((aktivitet.getFrilanser() != null)
+                    || ((aktivitet.getSelvstendigNæringsdrivende() != null) && !aktivitet.getSelvstendigNæringsdrivende().isEmpty())) {
+                feil.addAll(validerFrilanserOgSelvstendingNæringsdrivende(aktivitet.getSelvstendigNæringsdrivende(), aktivitet.getFrilanser()));
+                feil.addAll(validerSelvstendingNæringsdrivende(aktivitet.getSelvstendigNæringsdrivende()));
+                feil.addAll(validerFrilanser(aktivitet.getFrilanser()));
+            }
+        }
+        return feil;
+    }
+
+    private List<Feil> validerFrilanserOgSelvstendingNæringsdrivende(List<SelvstendigNæringsdrivende> selvstendigeVirksomheter, Frilanser frilanser) {
+        List<Feil> feil = new ArrayList<>();
         if (frilanser == null && (selvstendigeVirksomheter == null || selvstendigeVirksomheter.isEmpty())) {
             feil.add(new Feil("frilanser & selvstendingNæringsdrivene", PÅKREVD, "Enten frilanser eller selvstendingNæringsdrivende må være satt i søknaden."));
         }
+        return feil;
     }
 
-    // Spennende at denne ikke er i bruk .....
-    private void validerFrilanser(Frilanser frilanser, List<Feil> feil) {
-        if (frilanser == null) return;
+    private List<Feil> validerFrilanser(Frilanser frilanser) {
+        List<Feil> feil = new ArrayList<>();
+        if (frilanser == null) return feil;
 
-        if (frilanser.getSluttdato() != null && frilanser.getStartdato().isAfter(frilanser.getSluttdato())) {
+        if (frilanser.getStartdato() != null && frilanser.getSluttdato() != null && frilanser.getStartdato().isAfter(frilanser.getSluttdato())) {
             feil.add(new Feil("frilanser.startdato", UGYLDIG_ARGUMENT, "'startdato' kan ikke være etter 'sluttdato'"));
         }
+        return feil;
     }
 
-    private void validerSelvstendingNæringsdrivende(List<SelvstendigNæringsdrivende> selvstendigeVirksomheter, List<Feil> feil) {
+    private List<Feil> validerSelvstendingNæringsdrivende(List<SelvstendigNæringsdrivende> selvstendigeVirksomheter) {
+        List<Feil> feil = new ArrayList<>();
         if (selvstendigeVirksomheter == null || selvstendigeVirksomheter.isEmpty())
-            return;
+            return feil;
         var index = 0;
         for (SelvstendigNæringsdrivende sn : selvstendigeVirksomheter) {
             String snFelt = "selvstendigNæringsdrivende[" + index + "]";
@@ -91,5 +121,131 @@ public class OmsorgspengerUtbetalingValidator extends YtelseValidator {
             });
             index++;
         }
+        return feil;
+    }
+
+    private List<Feil> validerUtenlandsopphold(OmsorgspengerUtbetaling ytelse) {
+        var utenlandsopphold = ytelse.getUtenlandsopphold();
+        if (utenlandsopphold == null) {
+            return List.of();
+        }
+        return new PeriodeValidator().validerIkkeTillattOverlapp(utenlandsopphold.getPerioder(), "utenlandsopphold.perioder");
+    }
+
+    private List<Feil> validerFosterbarn(OmsorgspengerUtbetaling ytelse) {
+        var fosterbarn = ytelse.getFosterbarn();
+        if (fosterbarn == null || fosterbarn.isEmpty()) {
+            return List.of();
+        }
+        var index = 0;
+        List<Feil> feil = new ArrayList<>();
+        for (Barn b : fosterbarn) {
+            if (b.getPersonIdent() == null && b.getFødselsdato() == null) {
+                feil.add(new Feil("fosterbarn[" + index + "]", "norskIdentitetsnummerEllerFødselsdatoPåkrevd",
+                        "Må sette enten Personnummer/D-nummer på fosterbarn, eller fødselsdato."));
+            } else if (b.getPersonIdent() != null && b.getFødselsdato() != null) {
+                feil.add(
+                        new Feil("fosterbarn[" + index + "]", "ikkeEntydigIdPåBarnet", "Må sette enten Personnummer/D-nummer på fosterbarn, eller fødselsdato."));
+            }
+            index++;
+        }
+        return feil;
+    }
+
+    private List<Feil> validerBosteder(OmsorgspengerUtbetaling ytelse) {
+        var bosteder = ytelse.getBosteder();
+        if (bosteder == null) {
+            return List.of();
+        }
+        return new PeriodeValidator().validerIkkeTillattOverlapp(bosteder.getPerioder(), "bosteder.perioder");
+    }
+
+    private List<Feil> validerFraværskorrigeringIm(OmsorgspengerUtbetaling ytelse) {
+        var fraværsperioderKorrigeringIm = ytelse.getFraværsperioderKorrigeringIm();
+        if (fraværsperioderKorrigeringIm == null) {
+            return List.of();
+        }
+
+        var index = 0;
+        Organisasjonsnummer uniktOrgnr = null;
+        String unikArbeidsforholdId = null;
+        List<Feil> feil = new ArrayList<>();
+        for (FraværPeriode fraværPeriode : fraværsperioderKorrigeringIm) {
+            if (index == 0) {
+                // Initier verdier for arbeidsforhold som skal være konsistente for alle fraværsperioder
+                uniktOrgnr = fraværPeriode.getArbeidsgiverOrgNr();
+                unikArbeidsforholdId = fraværPeriode.getArbeidsforholdId();
+            }
+            if (fraværPeriode.getArbeidsgiverOrgNr() == null) {
+                feil.add(new Feil("fraværsperioderKorrigeringIm[" + index + "]", "ikkeSpesifisertOrgNr", "Må oppgi orgnr for aktivitet"));
+            } else if (!Objects.equals(fraværPeriode.getArbeidsgiverOrgNr(), uniktOrgnr)) {
+                feil.add(new Feil("fraværsperioderKorrigeringIm[" + index + "]", "ikkeUniktOrgNr", "Alle aktiviteter må ha samme orgnr"));
+            }
+            if (!Objects.equals(fraværPeriode.getArbeidsforholdId(), unikArbeidsforholdId)) {
+                feil.add(new Feil("fraværsperioderKorrigeringIm[" + index + "]", "ikkeUnikArbeidsforholdId", "Alle aktiviteter må ha samme orgnr"));
+            }
+
+            var aktiviteterFravær = fraværPeriode.getAktivitetFravær();
+            if (aktiviteterFravær.size() != 1) {
+                feil.add(new Feil("fraværsperioderKorrigeringIm[" + index + "]", "ikkeEntydigAktivitet", "Kan kun oppgi én aktivitet"));
+            } else if (aktiviteterFravær.iterator().next() != AktivitetFravær.ARBEIDSTAKER) {
+                feil.add(new Feil("fraværsperioderKorrigeringIm[" + index + "]", "ikkeGyldigAktivitet", "Aktivitet må være av type Arbeidstaker"));
+            }
+
+            if (Duration.ZERO.equals(fraværPeriode.getDuration()) && !fraværPeriode.getPeriode().getFraOgMed().equals(fraværPeriode.getPeriode().getTilOgMed())) {
+                feil.add(new Feil("fraværsperioderKorrigeringIm[" + index + "]", "nullingPeriodeOversteget", "Nulling av periode kan ikke ha lenger varighet enn én dag"));
+            }
+
+            if (fraværPeriode.getDuration() != null && fraværPeriode.getDuration().compareTo(Duration.parse("PT7H30M")) == 1) {
+                feil.add(new Feil("fraværsperioderKorrigeringIm[" + index + "]", "varighetOversteget", "Delvis fravær kan ikke overstige 7 timer og 30 min"));
+            }
+
+            index++;
+        }
+
+        // Valider perioder på tvers
+        var perioder = fraværsperioderKorrigeringIm.stream().collect(Collectors.toMap(e -> e.getPeriode(), e -> e.getArbeidsgiverOrgNr()));
+        feil.addAll(periodeValidator.validerIkkeTillattOverlapp(perioder, "fraværsperioderKorrigeringIm.perioder"));
+
+        return feil;
+    }
+
+    private List<Feil> validerFraværsperioderFraSøker(OmsorgspengerUtbetaling ytelse) {
+        var fraværsperioder = ytelse.getFraværsperioder();
+        if (fraværsperioder == null) {
+            return List.of();
+        }
+
+        var index = 0;
+        List<Feil> feil = new ArrayList<>();
+
+        for (FraværPeriode fraværPeriode : fraværsperioder) {
+            if (fraværPeriode.getAktivitetFravær() == null || fraværPeriode.getAktivitetFravær().isEmpty()) {
+                feil.add(new Feil("fraværsperioder[" + index + "]", "påkrevd", "Aktivitet må være satt"));
+            }
+            if (fraværPeriode.getÅrsak() == null) {
+                feil.add(new Feil("fraværsperioder[" + index + "]", "påkrevd", "Fraværsårsak må være satt"));
+            }
+            if (fraværPeriode.getSøknadÅrsak() == null) {
+                feil.add(new Feil("fraværsperioder[" + index + "]", "påkrevd", "Søknadårsak må være satt"));
+            }
+            index++;
+        }
+        return feil;
+    }
+
+    private List<Feil> validerFraværsperiodeKilder(OmsorgspengerUtbetaling ytelse) {
+        List<Feil> feil = new ArrayList<>();
+
+        var fraværsperioder = Optional.ofNullable(ytelse.getFraværsperioder()).orElse(List.of());
+        var fraværsperioderKorrigeringIm = Optional.ofNullable(ytelse.getFraværsperioderKorrigeringIm()).orElse(List.of());
+        ;
+
+        if (fraværsperioder.isEmpty() && fraværsperioderKorrigeringIm.isEmpty()) {
+            feil.add(new Feil("fraværsperioder", "ingenPerioder", "Må oppgi minst én fraværsperiode"));
+        } else if (!fraværsperioder.isEmpty() && !fraværsperioderKorrigeringIm.isEmpty()) {
+            feil.add(new Feil("fraværsperioder", "ugyldigKombinasjon", "Kan ikke korrigere både søknad og inntektsmelding i samme dokument"));
+        }
+        return feil;
     }
 }
