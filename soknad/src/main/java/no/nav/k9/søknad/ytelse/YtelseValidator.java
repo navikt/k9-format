6b1f2caf4309a8fa7538aab5c3c5bd222b84ce64
Stian Grenborgen <stian.grenborgen@nav.no>
2022-01-28 15:55:05 +0100

    Gjør det mulig å validere med endringsperiode uten å caste til spesifikk ytelsesvalidator.

--- soknad/src/main/java/no/nav/k9/søknad/ytelse/YtelseValidator.java
+++ soknad/src/main/java/no/nav/k9/søknad/ytelse/YtelseValidator.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 import no.nav.k9.søknad.ValideringsFeil;
 import no.nav.k9.søknad.felles.Feil;
+import no.nav.k9.søknad.felles.type.Periode;
 
 public abstract class YtelseValidator {
     protected static final String PÅKREVD = "påkrevd";
     protected static final String UGYLDIG_ARGUMENT = "ugyldig argument";
 
     public abstract List<Feil> valider(Ytelse søknad);
+    
+    public List<Feil> valider(Ytelse ytelse, List<Periode> gyldigeEndringsperioder) {
+        return valider(ytelse);
+    }
 
     public void forsikreValidert(Ytelse ytelse) {
         List<Feil> feil = valider(ytelse);
