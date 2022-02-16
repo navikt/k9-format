723364d065323128ad0e7941119b9e4aadd027fd
s149212 <espen.svendsen@nav.no>
2022-02-16 17:23:36 +0100

    Legger til pp livets sluttfase i ettersendelse

--- ettersendelse/src/main/java/no/nav/k9/ettersendelse/Ytelse.java
+++ ettersendelse/src/main/java/no/nav/k9/ettersendelse/Ytelse.java
@@ -2,20 +2,22 @@ package no.nav.k9.ettersendelse;
 
 public enum Ytelse {
     PLEIEPENGER_SYKT_BARN,
-    
+
+    PLEIEPENGER_LIVETS_SLUTTFASE,
+
     /** @deprecated av OMP_UT, OMP_UTV_KS, OMP_UTV_MA. */
     @Deprecated(forRemoval = true)
     OMSORGSPENGER,
-    
+
     /** Omsorgspenger utbetaling ytelse. */
     OMP_UT,
-    
+
     /** Omsorgspenger utvidet rett - kronisk syke eller funksjonshemming. */
     OMP_UTV_KS,
-    
+
     /** Omsorgspenger utvidet rett - midlertidig alene. */
     OMP_UTV_MA,
-    
+
     /** Omsorgspenger alene om omsorg. */
     OMP_UTV_AO,
 
