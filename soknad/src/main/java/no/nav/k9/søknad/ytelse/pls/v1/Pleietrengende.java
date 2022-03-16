ba5d224fe475f1346717262fe0ed17c032370af1
s149212 <espen.svendsen@nav.no>
2022-03-16 22:18:37 +0100

    Legger til fødselsdato på Pleietrengende for livets sluttfase

--- soknad/src/main/java/no/nav/k9/søknad/ytelse/pls/v1/Pleietrengende.java
+++ soknad/src/main/java/no/nav/k9/søknad/ytelse/pls/v1/Pleietrengende.java
@@ -1,13 +1,16 @@
 package no.nav.k9.søknad.ytelse.pls.v1;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
@@ -25,9 +28,35 @@ public class Pleietrengende implements Person {
     @Valid
     private NorskIdentitetsnummer norskIdentitetsnummer;
 
+    @JsonProperty(value = "fødselsdato", required = false)
+    @Valid
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Europe/Oslo")
+    @PastOrPresent(message = "[ugyldigFødselsdato] Fødselsdato kan ikke være fremtidig")
+    private LocalDate fødselsdato;
+
     @JsonCreator
-    public Pleietrengende(@JsonProperty(value = "norskIdentitetsnummer", required = true) @JsonAlias({ "fødselsnummer", "norskIdentifikator", "identitetsnummer", "fnr" }) NorskIdentitetsnummer norskIdentitetsnummer) {
+    public Pleietrengende(@JsonProperty(value = "norskIdentitetsnummer", required = true) @JsonAlias({ "fødselsnummer", "norskIdentifikator", "identitetsnummer", "fnr" }) NorskIdentitetsnummer norskIdentitetsnummer,
+                          @JsonProperty("fødselsdato") @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Europe/Oslo") LocalDate fødselsdato) {
         this.norskIdentitetsnummer = Objects.requireNonNull(norskIdentitetsnummer, "norskIdentitetsnummer");
+    }
+
+    @Override
+    public PersonIdent getPersonIdent() {
+        return norskIdentitetsnummer;
+    }
+
+    public LocalDate getFødselsdato() {
+        return fødselsdato;
+    }
+
+    public Pleietrengende medNorskIdentitetsnummer(NorskIdentitetsnummer norskIdentitetsnummer) {
+        this.norskIdentitetsnummer = norskIdentitetsnummer;
+        return this;
+    }
+
+    public Pleietrengende medFødselsdato(LocalDate fødselsdato) {
+        this.fødselsdato = fødselsdato;
+        return this;
     }
 
     @Override
@@ -38,17 +67,13 @@ public class Pleietrengende implements Person {
             return false;
         var other = (Pleietrengende) obj;
 
-        return Objects.equals(norskIdentitetsnummer, other.norskIdentitetsnummer);
-    }
-
-    @Override
-    public PersonIdent getPersonIdent() {
-        return norskIdentitetsnummer;
+        return Objects.equals(norskIdentitetsnummer, other.norskIdentitetsnummer)
+                && Objects.equals(fødselsdato, other.fødselsdato);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(norskIdentitetsnummer);
+        return Objects.hash(norskIdentitetsnummer, fødselsdato);
     }
 
 }
