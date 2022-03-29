7ce6ae0d0ae3f316535b8921941fff5465418cc6
s149212 <espen.svendsen@nav.no>
2022-03-29 08:30:15 +0200

    Gjør avhengighet til felles Pleiepenger-kode eksplisitt

--- soknad/src/main/java/no/nav/k9/søknad/ytelse/pls/v1/PleiepengerLivetsSluttfasePerioderMedEndringUtil.java
+++ soknad/src/main/java/no/nav/k9/søknad/ytelse/pls/v1/PleiepengerLivetsSluttfasePerioderMedEndringUtil.java
@@ -1,8 +1,6 @@
 package no.nav.k9.søknad.ytelse.pls.v1;
 
 import static no.nav.k9.søknad.TidsserieUtils.toLocalDateTimeline;
-import static no.nav.k9.søknad.ytelse.psb.v1.PerioderMedEndringUtil.getArbeidstidPerioder;
-import static no.nav.k9.søknad.ytelse.psb.v1.PerioderMedEndringUtil.tilTidsserie;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,12 +8,13 @@ import java.util.List;
 import no.nav.k9.søknad.TidsserieUtils;
 import no.nav.k9.søknad.felles.type.Periode;
 import no.nav.k9.søknad.ytelse.psb.v1.PerioderMedEndring;
+import no.nav.k9.søknad.ytelse.psb.v1.PerioderMedEndringUtil;
 
 public class PleiepengerLivetsSluttfasePerioderMedEndringUtil {
 
     public static List<Periode> getEndringsperiode(PleipengerLivetsSluttfase ppn) {
         var allePerioderMedEndringTidsserie =
-                tilTidsserie(getAllePerioderSomMåVæreInnenforSøknadsperiode(ppn));
+                PerioderMedEndringUtil.tilTidsserie(getAllePerioderSomMåVæreInnenforSøknadsperiode(ppn));
         var søknadsperiode = toLocalDateTimeline(ppn.getSøknadsperiodeList());
         var endringsperiodeTidsserie = allePerioderMedEndringTidsserie.disjoint(søknadsperiode);
         return TidsserieUtils.tilPeriodeList(endringsperiodeTidsserie);
@@ -24,7 +23,7 @@ public class PleiepengerLivetsSluttfasePerioderMedEndringUtil {
     public static List<PerioderMedEndring> getAllePerioderSomMåVæreInnenforSøknadsperiode(PleipengerLivetsSluttfase psb) {
         var listen = new ArrayList<PerioderMedEndring>();
         listen.add(new PerioderMedEndring().medPerioder("uttak", psb.getUttak().getPerioder()));
-        listen.addAll(getArbeidstidPerioder(psb.getArbeidstid()));
+        listen.addAll(PerioderMedEndringUtil.getArbeidstidPerioder(psb.getArbeidstid()));
         return listen;
     }
 
