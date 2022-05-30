b2abefea70e4af819df428528247fab818808de8
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2022-05-30 13:52:11 +0200

    Bumper tidsserie-versjon, og noen små forenklinger (#274)
    * Bumper tidsserie-versjon, og noen små forenklinger
    
    * final på klasser

--- innsyn/src/main/java/no/nav/k9/innsyn/Søknadsammenslåer.java
+++ innsyn/src/main/java/no/nav/k9/innsyn/Søknadsammenslåer.java
@@ -20,26 +20,29 @@ import no.nav.k9.søknad.ytelse.psb.v1.PleiepengerSyktBarn;
 import no.nav.k9.søknad.ytelse.psb.v1.tilsyn.TilsynPeriodeInfo;
 import no.nav.k9.søknad.ytelse.psb.v1.tilsyn.Tilsynsordning;
 
-public class Søknadsammenslåer {
-    
+public final class Søknadsammenslåer {
+
+    private Søknadsammenslåer() {
+    }
+
     public static Søknad kunPleietrengendedata(Søknad søknad) {
         if (!(søknad.getYtelse() instanceof PleiepengerSyktBarn)) {
             throw new IllegalArgumentException("Kun PSB-søknader er støttet.");
         }
         final PleiepengerSyktBarn ytelse = (PleiepengerSyktBarn) søknad.getYtelse();
-        
+
         final Map<Periode, TilsynPeriodeInfo> periodeMap = hentUtTilsynsperioder(ytelse);
         final PleiepengerSyktBarn resultatYtelse = new PleiepengerSyktBarn();
         resultatYtelse.medTilsynsordning(new Tilsynsordning().medPerioder(periodeMap));
-        
+
         return new Søknad().medYtelse(resultatYtelse);
     }
 
     private static Map<Periode, TilsynPeriodeInfo> hentUtTilsynsperioder(final PleiepengerSyktBarn ytelse) {
-        /* 
+        /*
          * Advarsel: Data som returneres her deles på tvers av søkere. Ikke legg inn
          *           flere felter i mappingen hvis de ikke skal deles.
-         *           
+         *
          *           Av denne grunn brukes ikke standard copy-contructor for Map/Tilsynordning
          */
         return ytelse.getTilsynsordning()
@@ -47,31 +50,31 @@ public class Søknadsammenslåer {
                 .entrySet()
                 .stream()
                 .map(e -> Map.entry(
-                    new Periode(e.getKey()),
-                    new TilsynPeriodeInfo().medEtablertTilsynTimerPerDag(e.getValue().getEtablertTilsynTimerPerDag())
+                        new Periode(e.getKey()),
+                        new TilsynPeriodeInfo().medEtablertTilsynTimerPerDag(e.getValue().getEtablertTilsynTimerPerDag())
                 ))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
-    
+
     public static Søknad slåSammen(Søknad tidligereSøknad, Søknad nySøknad) {
         if (!(tidligereSøknad.getYtelse() instanceof PleiepengerSyktBarn) || !(nySøknad.getYtelse() instanceof PleiepengerSyktBarn)) {
             throw new IllegalArgumentException("Kun PSB-søknader er støttet.");
         }
 
-        final PleiepengerSyktBarn s1Ytelse = (PleiepengerSyktBarn) tidligereSøknad.getYtelse();
-        final PleiepengerSyktBarn s2Ytelse = (PleiepengerSyktBarn) nySøknad.getYtelse();
-        
+        final PleiepengerSyktBarn s1Ytelse = tidligereSøknad.getYtelse();
+        final PleiepengerSyktBarn s2Ytelse = nySøknad.getYtelse();
+
         final PleiepengerSyktBarn ytelse = new PleiepengerSyktBarn();
         ytelse.medSøknadsperiode(slåSammenSøknadsperioder(s1Ytelse, s2Ytelse));
-        
+
         /* Bruk pleietrengendeAktørId fra PsbSøknadsinnhold fremfor: */
         ytelse.medBarn(new Barn()
-            .medNorskIdentitetsnummer(NorskIdentitetsnummer.of("00000000000"))
+                .medNorskIdentitetsnummer(NorskIdentitetsnummer.of("00000000000"))
         );
 
         ytelse.medArbeidstid(Arbeidstidsammenslåer.slåSammenArbeidstid(s1Ytelse, s2Ytelse));
         ytelse.medTilsynsordning(Tilsynsammenslåer.slåsammen(s1Ytelse, s2Ytelse));
-        
+
         final Søknad s = new Søknad()
                 .medSøknadId(SøknadId.of("generert"))
                 .medVersjon("1.0.0.")
@@ -86,10 +89,10 @@ public class Søknadsammenslåer {
     private static List<Periode> slåSammenSøknadsperioder(final PleiepengerSyktBarn s1Ytelse, final PleiepengerSyktBarn s2Ytelse) {
         final LocalDateTimeline<Boolean> t1 = TidsserieUtils.toLocalDateTimeline(s1Ytelse.getSøknadsperiodeList());
         final LocalDateTimeline<Boolean> t2 = TidsserieUtils.toLocalDateTimeline(s2Ytelse.getSøknadsperiodeList());
-        final List<Periode> søknadsperioder = TidsserieUtils.tilPeriodeList(t1
+        LocalDateTimeline<Boolean> harSøknadTidslinje = t1
                 .union(t2, StandardCombinators::coalesceRightHandSide)
                 .disjoint(TidsserieUtils.toLocalDateTimeline(s2Ytelse.getTrekkKravPerioder()))
-                .compress());
-        return søknadsperioder;
+                .compress();
+        return TidsserieUtils.tilPeriodeList(harSøknadTidslinje);
     }
 }
--- soknad/src/main/java/no/nav/k9/søknad/TidsserieUtils.java
+++ soknad/src/main/java/no/nav/k9/søknad/TidsserieUtils.java
@@ -7,24 +7,22 @@ import no.nav.fpsak.tidsserie.LocalDateSegment;
 import no.nav.fpsak.tidsserie.LocalDateTimeline;
 import no.nav.k9.søknad.felles.type.Periode;
 
-public class TidsserieUtils {
+public final class TidsserieUtils {
+    private TidsserieUtils() {
+    }
+
     public static List<Periode> tilPeriodeList(LocalDateTimeline<?> t) {
-        return t.stream().map(l -> new Periode(l.getFom(), l.getTom())).collect(Collectors.toList());
+        return t.stream()
+                .map(l -> new Periode(l.getFom(), l.getTom()))
+                .collect(Collectors.toList());
     }
 
     public static LocalDateTimeline<Boolean> toLocalDateTimeline(List<Periode> perioder) {
-        return new LocalDateTimeline<Boolean>(perioder
+        return new LocalDateTimeline<>(perioder
                 .stream()
-                .map(no.nav.k9.søknad.TidsserieUtils::mapLocalDateSegment)
+                .map(periode -> new LocalDateSegment<>(periode.getFraOgMed(), periode.getTilOgMed(), true))
                 .collect(Collectors.toList()))
                 .compress();
-    }
-
-    private static LocalDateSegment<Boolean> mapLocalDateSegment(Periode periode) {
-        return new LocalDateSegment<Boolean>(
-                periode.getFraOgMed(),
-                periode.getTilOgMed(),
-                true);
     }
 
 }
