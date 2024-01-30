d2c4831aae483f2b1db530700c0ead5c08aef4a3
Ramin Esfandiari <ramin.esfandiari@nav.no>
2024-01-30 13:44:32 +0100

    Legger til ny modul for konstanter. (#408)
    * Legger til ny modul for konstanter.
    
    Inneholder pr. nå forventet saksbehandlingstid.
    
    * Fikser versjon

---
+++ konstant/src/main/java/no/nav/k9/konstant/Konstant.java
@@ -0,0 +1,8 @@
+package no.nav.k9.konstant;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
+public class Konstant {
+    static final Duration FORVENTET_SAKSBEHANDLINGSTID = Duration.of(8, ChronoUnit.WEEKS);
+}
