1d6e49b0dba30d138440d2af68cc6b70485b3578
Qadeer Ahmad Khan <qadeer.ahmad.khan@nav.no>
2024-02-09 11:58:52 +0100

    endrer til å bruke journalpostid istedenfor søknadid da det er enklere å sende dette fra k9-sak og innsyn kan gjøre oppslag i sin database for å finne Søknad. Utvider med kildesystem for å skille mellom søknader som kommer fra endringsdialog og søknadsdialog

--- innsyn/src/main/java/no/nav/k9/innsyn/sak/SøknadInfo.java
+++ innsyn/src/main/java/no/nav/k9/innsyn/sak/SøknadInfo.java
@@ -1,11 +1,15 @@
 package no.nav.k9.innsyn.sak;
 
+import java.time.ZonedDateTime;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
-
-import java.time.ZonedDateTime;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import no.nav.k9.søknad.felles.Kildesystem;
 
 public record SøknadInfo(
 
@@ -14,15 +18,22 @@ public record SøknadInfo(
         @NotNull
         SøknadStatus status,
 
-        @JsonProperty(value = "søknadId", required = true)
+        @JsonProperty(value = "journalpostId", required = true)
         @Valid
         @NotNull
-        String søknadId,
+        @Size(max = 50, min = 3)
+        @Pattern(regexp = "^[\\\\p{Alnum}]+$", message = "journalpostId [${validatedValue}] matcher ikke tillatt pattern [{regexp}]")
+        String journalpostId,
 
         @JsonProperty(value = "mottattTidspunkt", required = true)
         @Valid
         @NotNull
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX", timezone = "UTC")
-        ZonedDateTime mottattTidspunkt
+        ZonedDateTime mottattTidspunkt,
+
+        @Valid
+        @JsonProperty(value = "kildesystem")
+        Kildesystem kildesystem
+
 ) {
 }
