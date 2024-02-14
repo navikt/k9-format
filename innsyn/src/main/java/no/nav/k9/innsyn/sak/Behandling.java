9f1b67b0ee562facb70c6ee041231da6bf07236d
Qadeer Ahmad Khan <qadeer.ahmad.khan@nav.no>
2024-02-14 14:50:30 +0100

    jsonformat på datoer

--- innsyn/src/main/java/no/nav/k9/innsyn/sak/Behandling.java
+++ innsyn/src/main/java/no/nav/k9/innsyn/sak/Behandling.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -17,6 +18,7 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import no.nav.k9.innsyn.InnsynHendelseData;
 import no.nav.k9.konstant.Konstant;
+import no.nav.k9.søknad.felles.DtoKonstanter;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, fieldVisibility = JsonAutoDetect.Visibility.ANY)
@@ -29,10 +31,12 @@ public record Behandling(
 
         @JsonProperty(value = "opprettetTidspunkt", required = true)
         @NotNull
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DtoKonstanter.DATO_TID_FORMAT, timezone = DtoKonstanter.TIDSSONE)
         ZonedDateTime opprettetTidspunkt,
 
         @JsonInclude(value = JsonInclude.Include.NON_NULL)
         @JsonProperty(value = "avsluttetTidspunkt")
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DtoKonstanter.DATO_TID_FORMAT, timezone = DtoKonstanter.TIDSSONE)
         ZonedDateTime avsluttetTidspunkt,
 
         @JsonInclude(value = JsonInclude.Include.NON_NULL)
