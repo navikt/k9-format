427fd2ae2650e886d48f4d6958d141ded1db90e6
Espen Velsvik <espen.velsvik@nav.no>
2025-01-29 13:47:49 +0100

    Setter deltakelsesøknad i test

--- soknad/src/test/java/no/nav/k9/søknad/ytelse/ung/YtelseEksempel.java
+++ soknad/src/test/java/no/nav/k9/søknad/ytelse/ung/YtelseEksempel.java
@@ -1,5 +1,6 @@
 package no.nav.k9.søknad.ytelse.ung;
 
+import no.nav.k9.søknad.ytelse.ung.v1.UngSøknadstype;
 import no.nav.k9.søknad.ytelse.ung.v1.Ungdomsytelse;
 
 import java.time.LocalDate;
@@ -9,6 +10,7 @@ public class YtelseEksempel {
     public static Ungdomsytelse komplettYtelseMedSøknadsperiode(LocalDate fraOgMed) {
 
         return new Ungdomsytelse()
-                .medStartdato(fraOgMed);
+                .medStartdato(fraOgMed)
+                .medSøknadType(UngSøknadstype.DELTAKELSE_SØKNAD);
     }
 }
