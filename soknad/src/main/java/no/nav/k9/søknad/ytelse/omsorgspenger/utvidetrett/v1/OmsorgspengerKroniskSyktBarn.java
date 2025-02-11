33d3aa9fd7b47e67705e417f1773487f43bf9b96
Audun Eltvik <audun.eltvik@nav.no>
2025-02-11 10:16:04 +0100

    Legg på validering

--- soknad/src/main/java/no/nav/k9/søknad/ytelse/omsorgspenger/utvidetrett/v1/OmsorgspengerKroniskSyktBarn.java
+++ soknad/src/main/java/no/nav/k9/søknad/ytelse/omsorgspenger/utvidetrett/v1/OmsorgspengerKroniskSyktBarn.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import no.nav.k9.søknad.felles.Feil;
 import no.nav.k9.søknad.felles.Versjon;
 import no.nav.k9.søknad.felles.personopplysninger.Barn;
@@ -38,11 +41,21 @@ public class OmsorgspengerKroniskSyktBarn implements OmsorgspengerUtvidetRett {
 
     @JsonProperty(value = "høyereRisikoForFraværBeskrivelse")
     @Valid
+    @Pattern(regexp = "^[\\p{Graph}\\p{Space}\\p{Sc}\\p{L}\\p{M}\\p{N}§]+$", message = "[${validatedValue}] matcher ikke tillatt pattern [{regexp}]")
+    @Size(min = 1, max = 1000, message = "Må være mellom 1 og 1000 tegn")
     private String høyereRisikoForFraværBeskrivelse;
 
     @JsonProperty(value = "dataBruktTilUtledning")
     @Valid
     private DataBruktTilUtledning dataBruktTilUtledning;
+
+    @AssertTrue(message = "høyereRisikoForFraværBeskrivelse er påkrevd dersom høyereRisikoForFraværBeskrivelse er true")
+    private boolean finnesBeskrivesleForHøyereRisikoForFravær() {
+        if(Boolean.TRUE.equals(høyereRisikoForFravær)) {
+            return høyereRisikoForFraværBeskrivelse != null;
+        }
+        return true;
+    }
 
     public OmsorgspengerKroniskSyktBarn() {
     }
