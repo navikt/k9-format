2521ec1218751465d56e3b10de5da55f9b592591
Espen Velsvik <espen.velsvik@nav.no>
2025-04-11 13:11:45 +0200

    TSFF-1375: Fjerner bekreftelser for endret startdato og endret sluttdato

--- oppgave-ungdomsytelse/src/test/java/no/nav/k9/oppgave/SerializeAndDeserializeTest.java
+++ oppgave-ungdomsytelse/src/test/java/no/nav/k9/oppgave/SerializeAndDeserializeTest.java
@@ -12,26 +12,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SerializeAndDeserializeTest {
 
     @Test
-    public void oppgave_bekreft_endret_startdato_serdes_test() {
-        var oppgaveBekreftelse = lagOppgaveBekreftelse(bekreftelseEndretStartdato(UUID.randomUUID(), LocalDate.now(), true));
-
-        var serializedOppgave = OppgaveBekreftelse.SerDes.serialize(oppgaveBekreftelse);
-        var deserilizedOppgave = OppgaveBekreftelse.SerDes.deserialize(serializedOppgave);
-
-        assertEquals(OppgaveBekreftelse.SerDes.serialize(deserilizedOppgave), serializedOppgave);
-    }
-
-    @Test
-    public void oppgave_bekreft_endret_sluttdato_serdes_test() {
-        var oppgaveBekreftelse = lagOppgaveBekreftelse(bekreftelseEndretSluttdatodato(UUID.randomUUID(), LocalDate.now(), true));
-
-        var serializedOppgave = OppgaveBekreftelse.SerDes.serialize(oppgaveBekreftelse);
-        var deserilizedOppgave = OppgaveBekreftelse.SerDes.deserialize(serializedOppgave);
-
-        assertEquals(OppgaveBekreftelse.SerDes.serialize(deserilizedOppgave), serializedOppgave);
-    }
-
-    @Test
     public void oppgave_bekreft_endret_programperiode_serdes_test() {
         var oppgaveBekreftelse = lagOppgaveBekreftelse(bekreftelseEndretProgramperiode(UUID.randomUUID(), new Periode(LocalDate.now(), LocalDate.now().plusDays(1)), true));
 
