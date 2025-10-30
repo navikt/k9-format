d690dfd80f0cabf504f8e811c72a43f58bfe9f7d
Morten Osvik <33130876+mortenoosvik@users.noreply.github.com>
2025-10-30 13:45:14 +0100

    Fjerner Inntektsmeldinger

--- innsyn/src/main/java/no/nav/k9/innsyn/inntektsmelding/Inntektsmelding.java
+++ innsyn/src/main/java/no/nav/k9/innsyn/inntektsmelding/Inntektsmelding.java
@@ -5,41 +5,36 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
+import no.nav.k9.innsyn.InnsynHendelseData;
 import no.nav.k9.innsyn.sak.FagsakYtelseType;
 import no.nav.k9.innsyn.sak.Saksnummer;
 
-
+@JsonTypeName(InnsynHendelseData.INNTEKTSMELDINGER)
 public record Inntektsmelding(
         @JsonProperty(value = "status", required = true)
-        @NotNull
         InntektsmeldingStatus status,
 
         @JsonProperty(value = "saksnummer", required = true)
         @Valid
-        @NotNull
         Saksnummer saksnummer,
 
         @JsonProperty(value = "graderinger", required = true)
         @Valid
-        @NotNull
         List<Gradering> graderinger,
 
         @JsonProperty(value = "naturalYtelser", required = true)
         @Valid
-        @NotNull
         List<NaturalYtelse> naturalYtelser,
 
         @JsonProperty(value = "utsettelsePerioder", required = true)
         @Valid
-        @NotNull
         List<UtsettelsePeriode> utsettelsePerioder,
 
         @JsonProperty(value = "arbeidsgiver", required = true)
         @Valid
-        @NotNull
         Arbeidsgiver arbeidsgiver,
 
         @JsonProperty(value = "startDatoPermisjon")
@@ -47,38 +42,30 @@ public record Inntektsmelding(
 
         @JsonProperty(value = "oppgittFravær", required = true)
         @Valid
-        @NotNull
         List<PeriodeAndel> oppgittFravær,
 
         @JsonProperty(value = "nærRelasjon", required = true)
-        @NotNull
         boolean nærRelasjon,
 
         @JsonProperty(value = "journalpostId", required = true)
         @Valid
-        @NotNull
         JournalpostId journalpostId,
 
         @JsonProperty(value = "mottattDato", required = true)
-        @NotNull
         LocalDate mottattDato,
 
         @JsonProperty(value = "inntektBeløp", required = true)
         @Valid
-        @NotNull
         Beløp inntektBeløp,
 
         @JsonProperty(value = "refusjonBeløpPerMnd", required = true)
         @Valid
-        @NotNull
         Beløp refusjonBeløpPerMnd,
 
         @JsonProperty(value = "refusjonOpphører", required = true)
-        @NotNull
         LocalDate refusjonOpphører,
 
         @JsonProperty(value = "innsendingstidspunkt", required = true)
-        @NotNull
         LocalDateTime innsendingstidspunkt,
 
         @JsonProperty(value = "kildesystem", required = true)
@@ -89,7 +76,6 @@ public record Inntektsmelding(
 
         @JsonProperty(value = "endringerRefusjon", required = true)
         @Valid
-        @NotNull
         List<Refusjon> endringerRefusjon,
 
         @JsonProperty(value = "innsendingsårsak", required = true)
@@ -100,10 +86,8 @@ public record Inntektsmelding(
 
         @JsonProperty(value = "erstattetAv", required = true)
         @Valid
-        @NotNull
         List<JournalpostId> erstattetAv
 
-)  {
-
+) implements InnsynHendelseData {
 }
 
